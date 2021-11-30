be85d7c774527809aea072e5150c93043bda0ab9
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2021-11-30 16:00:23 +0100

    storage images

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,6 +4,9 @@ const Card = () => {
     return (
         <div className="card">
             <div className="card__container">
+                <div className="card__image">
+                    <img src={process.env.PUBLIC_URL + "assets/imgs/img1.jpg"} alt="card"/>
+                </div>
                 <div className="card__like">
                     <button className="btn_like">
                     </button>

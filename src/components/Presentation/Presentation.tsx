0022ccd6e7cea224e3d1a1b0a3ed5a87df27be4a
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2021-12-05 11:05:37 +0100

    update pres and welcome component

--- src/components/Presentation/Presentation.tsx
+++ src/components/Presentation/Presentation.tsx
@@ -1,38 +1,70 @@
-import React, { useEffect, useRef, useState} from 'react';
-import "./Presentation.css"
-import Tilt from 'react-parallax-tilt';
+import React from "react";
+import "./Presentation.css";
+import Tilt from "react-parallax-tilt";
 
 const Presentation = () => {
   return (
-      <div id="anchor" className="background">
-        <div className="bodyPresentation">
-          <Tilt
-                          className="track-on-window"
-                          perspective={500}
-                          scale={1.0}
-                          tiltMaxAngleX={4}
-                          tiltMaxAngleY={4}
-                          trackOnWindow={true}
-                      >
-              <img src={process.env.PUBLIC_URL + "assets/imgs/purple_women.png"} alt='Women picture' className="women-picture"/>
-          </Tilt>
+    <section id="pres" className="section pres">
+      <div className="pres__wrap container">
+        <Tilt
+          className="track-on-window"
+          perspective={500}
+          scale={1.0}
+          tiltMaxAngleX={4}
+          tiltMaxAngleY={4}
+          trackOnWindow={true}
+        >
+          <img
+            src={process.env.PUBLIC_URL + "assets/imgs/purple_women.png"}
+            alt="Women picture"
+            className="women-picture"
+          />
+        </Tilt>
 
-          <div className="text-explanation">
-            <h3>Permettez à la nouvelle étoile montante de la musique de se démarquer.</h3>
-            <div className="explanation">
-              <p>
-                 Chaque semaine, le vainqueur de notre concours <strong>ROOTS®</strong> sera mis en avant sur nos différents réseaux ainsi que sur ceux de nos partenaires officiels, <strong>Spotify</strong> et <strong>RTL.</strong>
-              </p>
+        <div className="text-explanation">
+          <h3>
+            Permettez à la nouvelle étoile montante de la musique de se
+            démarquer.
+          </h3>
+          <div className="explanation">
+            <p>
+              Chaque semaine, le vainqueur de notre concours{" "}
+              <strong>ROOTS®</strong> sera mis en avant sur nos différents
+              réseaux ainsi que sur ceux de nos partenaires officiels,{" "}
+              <strong>Spotify</strong> et <strong>RTL.</strong>
+            </p>
 
-              <div className="logos">
-                <img src={process.env.PUBLIC_URL + "assets/imgs/spotify_logo.svg"} alt='Spotify logo'/>
-                <img src={process.env.PUBLIC_URL + "assets/imgs/RTL.svg"} alt='Rtl logo'/>
-                <img src={process.env.PUBLIC_URL + "assets/imgs/waramLogo.svg"} alt='Waram Logo' className="smallLogo"/>
-              </div>
+            <div className="logos">
+              <img
+                src={process.env.PUBLIC_URL + "assets/imgs/spotify_logo.svg"}
+                alt="Spotify logo"
+              />
+              <img
+                src={process.env.PUBLIC_URL + "assets/imgs/RTL.svg"}
+                alt="Rtl logo"
+              />
+              <img
+                src={process.env.PUBLIC_URL + "assets/imgs/waramLogo.svg"}
+                alt="Waram Logo"
+                className="smallLogo"
+              />
             </div>
           </div>
         </div>
       </div>
+      <div className="pres_bg cercle">
+        <img
+          src={process.env.PUBLIC_URL + "assets/imgs/cercle_bg_p.png"}
+          alt="forme"
+        />
+      </div>
+      <div className="pres_bg rectangle">
+        <img
+          src={process.env.PUBLIC_URL + "assets/imgs/rectangle_bg_p.png"}
+          alt="forme"
+        />
+      </div>
+    </section>
   );
 };
 
--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,125 +1,118 @@
-import React, { useEffect, useRef, useState} from 'react';
-import "./Welcome.css"
-import Tilt from 'react-parallax-tilt';
+import React from "react";
+import "./Welcome.css";
+import Tilt from "react-parallax-tilt";
 
 const Welcome = () => {
-const img = useRef(null)
-// const [wave, setWave] = useState(false);
-// let waveValue = 0;
-
-// waveValue = (window.scrollY/10);
-// console.log(waveValue)
-// const str = waveValue.toString();
-// const changeWave = () =>{
-//     if(window.scrollY >=80) {
-//         setWave(true);
-//     } else {
-//         setWave(false);
-//     }
-// }
-
-// window.addEventListener('scroll', changeWave)
-
-    return (
-        <section className="body-welcome">
-             <div className="content_welcome">
-
-{/* ###
-                    Title
-### */}
-
-                <div className="title_artist">
-                    <Tilt tiltReverse={true}
-                        className="track-on-window"
-                        perspective={500}
-                        scale={1.0}
-                        tiltMaxAngleX={4}
-                        tiltMaxAngleY={4}
-                        trackOnWindow={true}
-                    >
-                        <div className="drive-text">
-                            <span>Propulsez</span>
-                        </div>
-                        <div className="artist-text">
-                            <span>votre <span className="white-text">artiste</span></span>
-                        </div>
-                        <div className="favorite-text">
-                            <span>préféré</span>
-                        </div>
-                    </Tilt>
-                </div>
-
-{/* ###
-                    Albums
-### */}
-
-                <div className="albums inner-element">
-                    <div className="first-row">
-                    <Tilt tiltReverse={true}
-                        className="track-on-window"
-                        perspective={500}
-                        scale={1.0}
-                        tiltMaxAngleX={4}
-                        tiltMaxAngleY={4}
-                        trackOnWindow={true}
-                    >
-                        <img src={process.env.PUBLIC_URL + "assets/album/XXIVAlbum.png"} alt="XXIV Album" className="XXIVAlbum inner-element"/>
-                    </Tilt>
-                    <Tilt tiltReverse={true}
-                        className="track-on-window"
-                        perspective={500}
-                        scale={1.0}
-                        tiltMaxAngleX={4}
-                        tiltMaxAngleY={4}
-                        trackOnWindow={true}
-
-                    >
-                        <img src={process.env.PUBLIC_URL + "assets/album/platypusAlbum.png"} alt="Platypus Album" className="platypusAlbum inner-element"/>
-                    </Tilt>
-                    </div>
-                    <div className="second-row">
-                    <Tilt tiltReverse={true}
-                        className="track-on-window"
-                        perspective={500}
-                        scale={1.0}
-                        tiltMaxAngleX={4}
-                        tiltMaxAngleY={4}
-                        trackOnWindow={true}
-
-                    >
-                        <img src={process.env.PUBLIC_URL + "assets/album/faceAlbum.png"} alt="Vertigo Album" className="faceAlbum inner-element"/>
-                    </Tilt>
-                    <Tilt tiltReverse={true}
-                        className="track-on-window"
-                        perspective={500}
-                        scale={1.0}
-                        tiltMaxAngleX={4}
-                        tiltMaxAngleY={4}
-                        trackOnWindow={true}
-
-                    >
-                        <img src={process.env.PUBLIC_URL + "assets/album/donutAlbum.png"} alt="Donut Album" className="donutAlbum inner-element"/>
-                     </Tilt>
-                    </div>
-                </div>
-
-{/* ###
-                    Button
-### */}
-
-                <a className="progress" href="#anchor">
-                    <div className="waves_button">
-                        <div className="wave"
-                        //  style={{
-                        //     height: wave ? `${(85 + str)}%`: "85px"
-                        // }}
-                          ></div>
-                    </div>
-                    <img src={process.env.PUBLIC_URL + "assets/imgs/arrow_vector.svg"} alt="arrow" className="arrow"/>
-                </a>
+  return (
+    <section className="welcome">
+      <div className="content_welcome">
+        {/* ###Title### */}
+        <div className="title_artist">
+          <Tilt
+            tiltReverse={true}
+            className="track-on-window"
+            perspective={500}
+            scale={1.0}
+            tiltMaxAngleX={4}
+            tiltMaxAngleY={4}
+            trackOnWindow={true}
+          >
+            <div className="drive-text">
+              <span>Propulsez</span>
             </div>
-        </section>
-    );
+            <div className="artist-text">
+              <span>
+                votre <span className="white-text">artiste</span>
+              </span>
+            </div>
+            <div className="favorite-text">
+              <span>préféré</span>
+            </div>
+          </Tilt>
+        </div>
+        {/* ### Albums### */}
+        <div className="albums inner-element">
+          <div className="first-row">
+            <Tilt
+              tiltReverse={true}
+              className="track-on-window"
+              perspective={500}
+              scale={1.0}
+              tiltMaxAngleX={4}
+              tiltMaxAngleY={4}
+              trackOnWindow={true}
+            >
+              <img
+                src={process.env.PUBLIC_URL + "assets/album/XXIVAlbum.png"}
+                alt="XXIV Album"
+                className="XXIVAlbum inner-element"
+              />
+            </Tilt>
+            <Tilt
+              tiltReverse={true}
+              className="track-on-window"
+              perspective={500}
+              scale={1.0}
+              tiltMaxAngleX={4}
+              tiltMaxAngleY={4}
+              trackOnWindow={true}
+            >
+              <img
+                src={process.env.PUBLIC_URL + "assets/album/platypusAlbum.png"}
+                alt="Platypus Album"
+                className="platypusAlbum inner-element"
+              />
+            </Tilt>
+          </div>
+          <div className="second-row">
+            <Tilt
+              tiltReverse={true}
+              className="track-on-window"
+              perspective={500}
+              scale={1.0}
+              tiltMaxAngleX={4}
+              tiltMaxAngleY={4}
+              trackOnWindow={true}
+            >
+              <img
+                src={process.env.PUBLIC_URL + "assets/album/faceAlbum.png"}
+                alt="Vertigo Album"
+                className="faceAlbum inner-element"
+              />
+            </Tilt>
+            <Tilt
+              tiltReverse={true}
+              className="track-on-window"
+              perspective={500}
+              scale={1.0}
+              tiltMaxAngleX={4}
+              tiltMaxAngleY={4}
+              trackOnWindow={true}
+            >
+              <img
+                src={process.env.PUBLIC_URL + "assets/album/donutAlbum.png"}
+                alt="Donut Album"
+                className="donutAlbum inner-element"
+              />
+            </Tilt>
+          </div>
+        </div>
+        <div className="welcome_block_btn">
+          <a href="#pres">
+            <button className="welcome__btn">
+              <div className="welcome_btn_container_img">
+                <img
+                  src={process.env.PUBLIC_URL + "assets/icons/arrow_down.svg"}
+                  alt="arrow"
+                  className="welcome_btn_img"
+                />
+              </div>
+            </button>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
